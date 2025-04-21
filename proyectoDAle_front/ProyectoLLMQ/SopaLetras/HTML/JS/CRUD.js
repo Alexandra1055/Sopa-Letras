58b5a0872a8343fa5bb7b7e6622becb733e48c50
Pau Febrer Castro <pfebrerc@gmail.com>
2025-04-21 16:37:42 +0200

    Creació de contacto.js

--- proyectoDAle_front/ProyectoLLMQ/SopaLetras/HTML/JS/CRUD.js
+++ proyectoDAle_front/ProyectoLLMQ/SopaLetras/HTML/JS/CRUD.js
@@ -1,32 +1,39 @@
 /* Create */
 async function create(query) {
-    return await ejecutarQuery(query, "Se ha creado con exito el registro", "Error al crear el registro");
-  }
+  return await ejecutarQuery(query, "Se ha creado con éxito el registro", "Error al crear el registro");
+}
+
 /* Read */
 async function read(query) {
-    return await ejecutarQuery(query, null, "Error al leer los datos");
-  }
+  return await ejecutarQuery(query, null, "Error al leer los datos");
+}
+
 /* Update */
 async function update(query) {
-    return await ejecutarQuery(query, "Se ha actualizado con exito", "Error al actualizar el registro");
-  }
+  return await ejecutarQuery(query, "Se ha actualizado con éxito", "Error al actualizar el registro");
+}
+
 /* Delete */
 async function remove(query) {
-    return await ejecutarQuery(query, "Se ha eliminado con exito", "Error al eliminar el registro");
-  }
+  return await ejecutarQuery(query, "Se ha eliminado con éxito", "Error al eliminar el registro");
+}
 
 /* Ejecutar la Query */
 async function ejecutarQuery(query, mensajeExito, mensajeError) {
-    try {
-      const respuesta = await fetch(`http://localhost:3000/daw/${encodeURIComponent(query)}`);
-      const data = await respuesta.json();
-      console.log("Respuesta del servidor: ", data);
-      if (mensajeExito) 
+  try {
+    const respuesta = await fetch(`http://localhost:3000/daw/${encodeURIComponent(query)}`);
+    const data = await respuesta.json();
+    console.log("Respuesta del servidor: ", data);
+    if (respuesta.ok && data) {
+      if (mensajeExito) {
         alert(mensajeExito);
+      }
       return data;
-    } catch (error) {
-      console.error("Error en la operación: ", error);
-      alert(mensajeError || "Error en la operación");
+    } else {
+      throw new Error(data.error || mensajeError || "Error en la operación");
     }
+  } catch (error) {
+    console.error("Error en la operación: ", error);
+    throw new Error(mensajeError || "Error en la operación");
   }
-     
+}
